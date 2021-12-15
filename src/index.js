cb13337e3b6121aa11a69502d245fdf455260ca2
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-12-15 22:55:04 +0100

    Chore ⚙: Setup scrapper to capture fullpage screen

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ const puppeteer = require('puppeteer');
 
 const app = express();
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://learnwebcode.github.io/practice-requests');
-  // To take a screensot of a page
-  await page.screenshot({ path: 'screenshot.png' });
-  await browser.close();
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(
+      'https://www.uplead.com/reviews/code-red-safety-case-study/'
+    );
+    // To take a screensot of a page
+    await page.screenshot({ path: 'screenshot.png', fullPage: true }); // setting fullPage to true will take the screenshot of the full page length instead of taking the screenshot of a certain browser window
+    await browser.close();
+  } catch (err) {
+    console.log(err.message);
+  }
 })();
 module.exports = app;
