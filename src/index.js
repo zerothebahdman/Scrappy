31c6a5c0142b46345ef627ee740477bb806f1e7d
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-12-15 22:17:29 +0100

    Chore ⚙: Added Puppeteer for web scrapping.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const puppeteer = require('puppeteer');
 
 const app = express();
-
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://learnwebcode.github.io/practice-requests');
+  // To take a screensot of a page
+  await page.screenshot({ path: 'screenshot.png' });
+  await browser.close();
+})();
 module.exports = app;
